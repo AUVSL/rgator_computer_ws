cfdc82253f6d84a46720368f11c8a963dd7c059a
sdekhterman <sdekhterman@gmail.com>
2024-10-17 18:11:06 -0500

    fixed steering in output test
    The steering now has the right range of values.

--- dbw/test/test_ouput_range.py
+++ dbw/test/test_ouput_range.py
@@ -2,16 +2,23 @@
 
 import numpy as np
 
-lin_vel_min       = -250  # km/hr
 lin_vel_step_size = 1/128
-lin_spd_eff_min   = -40   # km/h
-lin_spd_eff_max   =  40   # km/h
+lin_vel_min       = -250  # km/hr
+lin_vel_eff_min   = -40   # km/h
+lin_vel_eff_max   =  40   # km/h
+
+ang_vel_step_size = 1/1024
+ang_vel_min       = -31.25 # rad/s
+ang_vel_eff_max   =  31.25 # rad/s
+
 percent_offset    = 100.0
 percent_range     = 200.0
 
-output_offset = abs(lin_vel_min / lin_vel_step_size)
-print(output_offset)
-lin_spd_range = (lin_spd_eff_max - lin_spd_eff_min) / lin_vel_step_size
+lin_offset    = abs(lin_vel_min / lin_vel_step_size)
+lin_spd_range = (lin_vel_eff_max - lin_vel_eff_min) / lin_vel_step_size
+
+ang_offset    = abs(ang_vel_min / ang_vel_step_size)
+ang_spd_range = (ang_vel_eff_max - ang_vel_min) / ang_vel_step_size
 
 throttle_cmd_new = []
 steering_cmd_new = []
@@ -24,18 +31,18 @@ for i in range(12):
 
     if gear == 1:
         print('gear: forward')
-        throttle_cmd = np.uint16(lin_spd_range * ( throttle_percentage + percent_offset) / percent_range + output_offset)
+        throttle_cmd = np.uint16(lin_spd_range * ( throttle_percentage + percent_offset) / percent_range + lin_offset)
         throttle_cmd_new.append(throttle_cmd)
     elif gear == 2:
         print('gear: backward')
-        throttle_cmd = np.uint16(lin_spd_range * (-throttle_percentage + percent_offset) / percent_range + output_offset)
+        throttle_cmd = np.uint16(lin_spd_range * (-throttle_percentage + percent_offset) / percent_range + lin_offset)
         throttle_cmd_new.append(throttle_cmd)
     else:
         print('gear: neutral')
-        throttle_cmd = np.uint16(lin_spd_range * (                  0 + percent_offset) / percent_range + output_offset)
+        throttle_cmd = np.uint16(lin_spd_range * (                  0 + percent_offset) / percent_range + lin_offset)
         throttle_cmd_new.append(throttle_cmd)
 
-    steering_cmd     = np.uint16(lin_spd_range * (steering_percentage + percent_offset) / percent_range + output_offset)
+    steering_cmd     = np.uint16(ang_spd_range * (steering_percentage + percent_offset) / percent_range)
     steering_cmd_new.append(steering_cmd)
 
 print(throttle_cmd_new)
