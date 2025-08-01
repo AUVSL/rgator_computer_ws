e84a1bc0f6a6df0b217047eccdbd7953ad6a23eb
sdekhterman <varunrpj@gmail.com>
2025-08-01 14:50:05 -0500

    working dbvw_vel
    for some reason this was never uploaded

--- dbw/dbw/dbw_vel.py
+++ dbw/dbw/dbw_vel.py
@@ -23,28 +23,25 @@ class DBW(Node):
         self.count = 0
         self.lin_vel_step_size = 1/128
         self.lin_vel_min       = -250  # km/hr
-        self.lin_vel_eff_max   =  32   # km/h
+        self.lin_vel_eff_max   =  40   # km/h
 
         self.ang_vel_step_size = 1/1024
         self.ang_vel_min       = -31.25 # rad/s
-        self.ang_vel_eff_min   =  0.5 # rad/s
-        self.ang_vel_eff_max   =  0.5 # rad/s
+        self.ang_vel_eff_max   = 0.5    # rad/s
 
-        self.lin_percent_range  = 100.0
-        self.ang_percent_range  = 200.0
-        self.ang_percent_offset = 100.0
+        self.percent_dist  = 100.0
 
-        self.lin_offset    = abs(self.lin_vel_min / self.lin_vel_step_size)
-        self.lin_spd_range = self.lin_vel_eff_max / self.lin_vel_step_size
+        self.lin_offset   = abs(self.lin_vel_min / self.lin_vel_step_size)
+        self.lin_spd_dist = self.lin_vel_eff_max / self.lin_vel_step_size
 
-        self.ang_offset    = abs(self.ang_vel_min / self.ang_vel_step_size)
-        self.ang_spd_range = (self.ang_vel_eff_max - self.ang_vel_eff_min) / self.ang_vel_step_size
+        self.ang_offset   = abs(self.ang_vel_min / self.ang_vel_step_size)
+        self.ang_spd_dist = self.ang_vel_eff_max / self.ang_vel_step_size
 
         # can messages definations
         # sent messages (arbitration_id = priority 0x18 & pgn # in hexidecimal & device address in hexicemial, data = initial hex command byte, the remaining bits are defaults with unassigned bytes being set to 0xFF or 0x00 arbitrary
         self.addressClaim = can.Message(arbitration_id=0x18EEFF2A, data=[0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80], is_extended_id=True)
         self.heartBeat    = can.Message(arbitration_id=0x18FFFF2A, data=[0x80, 0x31, 0x5D, 0x55, 0xFF, 0xFD, 0xFF, 0xFF], is_extended_id=True)
-        self.propulsion   = can.Message(arbitration_id=0x18FFFF2A, data=[0x81, 0x7D, 0x00, 0x7D, 0x00, 0xFF, 0xFF, 0xFF], is_extended_id=True)
+        self.propulsion   = can.Message(arbitration_id=0x18FFFF2A, data=[0x81, 0x7D, 0x00, 0x7D, 0x00, 0x00, 0x00, 0x00], is_extended_id=True)
         self.inhibitCmd   = can.Message(arbitration_id=0x0CFE5A2A, data=[0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0xFF, 0xFF, 0xFF], is_extended_id=True)
 
         # send address claim message
@@ -56,6 +53,7 @@ class DBW(Node):
         self.count += 1
         if self.count == 10:
             self.bus.send(self.heartBeat)
+            print("sending heartbeat")
             self.count = 0
 
     def set_2_byte_number(self, byte_list, number, index):
@@ -70,15 +68,15 @@ class DBW(Node):
         if number < 0 or number > 65535:
             raise ValueError("Number must be between 0 and 65535")
 
-        byte_list[index]     = (number >> 8) & 0xFF # High byte
-        byte_list[index + 1] =        number & 0xFF # Low byte
+        byte_list[index+1] = (number >> 8) & 0xFF # High byte
+        byte_list[index]   =        number & 0xFF # Low byte
 
 
     def dbw_callback(self, msg):
-        inhibit             = msg.parkbrake     # 1 for lock; 0 for unlock
-        gear                = msg.gear          # 0 for neutral; 1 for forward; 2 for backward
-        throttle_percentage = msg.throttle      # 0 ~ 100 (%)
-        steering_percentage = msg.steering      # -100 ~ 100 (%), right-, left+
+        inhibit             = msg.parkbrake # 1 for lock; 0 for unlock
+        gear                = msg.gear      # 0 for neutral; 1 for forward; 2 for backward
+        throttle_percentage = msg.throttle  # 0 ~ 100 (%)
+        steering_percentage = msg.steering  # -100 ~ 100 (%), left+, right-
 
         if throttle_percentage < 0:
             throttle_percentage = 0
@@ -86,7 +84,6 @@ class DBW(Node):
         elif throttle_percentage > 100:
             throttle_percentage = 100
             print("WARNING: throttle percentage above range")
-
         if steering_percentage < -100:
             steering_percentage = -100
             print("WARNING: steering percentage below range")
@@ -96,21 +93,18 @@ class DBW(Node):
 
         if gear == 1:
             print('gear: forward')
-            throttle_cmd = np.uint16(self.lin_spd_range *  throttle_percentage / self.lin_percent_range + self.lin_offset)
+            throttle_cmd = np.uint16(self.lin_spd_dist *  throttle_percentage / self.percent_dist + self.lin_offset)
         elif gear == 2:
             print('gear: backward')
-            throttle_cmd = np.uint16(self.lin_spd_range * -throttle_percentage / self.lin_percent_range + self.lin_offset)
+            throttle_cmd = np.uint16(self.lin_spd_dist * -throttle_percentage / self.percent_dist + self.lin_offset)
         else:
             print('gear: neutral')
             throttle_cmd = np.uint16(self.lin_offset)
+        steering_cmd     = np.uint16(self.ang_spd_dist *  steering_percentage / self.percent_dist + self.ang_offset)
 
-        steering_cmd = np.uint16(self.ang_spd_range * (steering_percentage + self.ang_percent_offset) / self.ang_percent_range)
+        self.set_2_byte_number(self.propulsion.data, steering_cmd, 1)
 
-        # throttle
-        self.set_2_byte_number(self.propulsion.data, throttle_cmd, 1)
-
-        # steering
-        self.set_2_byte_number(self.propulsion.data, steering_cmd, 3)
+        self.set_2_byte_number(self.propulsion.data, throttle_cmd, 3)
 
         if inhibit == 1:
             self.inhibitCmd.data[4] = 0x10
